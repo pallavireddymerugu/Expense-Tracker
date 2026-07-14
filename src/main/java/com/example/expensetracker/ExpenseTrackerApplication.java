dcd537726bbc97488d967d31dd6150d6425cb0b0
pallavireddymerugu <merugupallavi45@gmail.com>
2026-07-14 13:36:30 +0530

    created JPA entities and configured PostgreSQL

--- src/main/java/com/example/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/example/expensetracker/ExpenseTrackerApplication.java
@@ -1,15 +1,13 @@
 package com.example.expensetracker;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ExpenseTrackerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ExpenseTrackerApplication.class, args);
     }
 
